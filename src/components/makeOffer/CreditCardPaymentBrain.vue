ac550e21ebfc17551dc44aa9219f034477e44e18
Francisco de Paz <frandepaz@yahoo.es>
2019-04-11 18:11:18 +0200

    Create offer almost done

--- src/components/makeOffer/CreditCardPaymentBrain.vue
+++ src/components/makeOffer/CreditCardPaymentBrain.vue
@@ -40,18 +40,64 @@ export default {
         return {
             gsecurity: GSecurity,
             name: undefined,
-            auth_key: "eyJ2ZXJzaW9uIjoyLCJhdXRob3JpemF0aW9uRmluZ2VycHJpbnQiOiI0NzBkODdiOWE3ZDc5ZGY3ODYxOGFhZDZiMWFkMmViZmMxYTNmMmQwMjc2NDJiMjlhYzIwZjBlY2Q4Njc0ZjQyfGNyZWF0ZWRfYXQ9MjAxOS0wNC0xMFQxMTo1NTozMC43OTUzMzYyNzQrMDAwMFx1MDAyNm1lcmNoYW50X2lkPTJqcjR6NnF6NHJmNG41eHRcdTAwMjZwdWJsaWNfa2V5PWRxZ3drY3NtYjdqNjh3ZjciLCJjb25maWdVcmwiOiJodHRwczovL2FwaS5zYW5kYm94LmJyYWludHJlZWdhdGV3YXkuY29tOjQ0My9tZXJjaGFudHMvMmpyNHo2cXo0cmY0bjV4dC9jbGllbnRfYXBpL3YxL2NvbmZpZ3VyYXRpb24iLCJncmFwaFFMIjp7InVybCI6Imh0dHBzOi8vcGF5bWVudHMuc2FuZGJveC5icmFpbnRyZWUtYXBpLmNvbS9ncmFwaHFsIiwiZGF0ZSI6IjIwMTgtMDUtMDgifSwiY2hhbGxlbmdlcyI6W10sImVudmlyb25tZW50Ijoic2FuZGJveCIsImNsaWVudEFwaVVybCI6Imh0dHBzOi8vYXBpLnNhbmRib3guYnJhaW50cmVlZ2F0ZXdheS5jb206NDQzL21lcmNoYW50cy8yanI0ejZxejRyZjRuNXh0L2NsaWVudF9hcGkiLCJhc3NldHNVcmwiOiJodHRwczovL2Fzc2V0cy5icmFpbnRyZWVnYXRld2F5LmNvbSIsImF1dGhVcmwiOiJodHRwczovL2F1dGgudmVubW8uc2FuZGJveC5icmFpbnRyZWVnYXRld2F5LmNvbSIsImFuYWx5dGljcyI6eyJ1cmwiOiJodHRwczovL29yaWdpbi1hbmFseXRpY3Mtc2FuZC5zYW5kYm94LmJyYWludHJlZS1hcGkuY29tLzJqcjR6NnF6NHJmNG41eHQifSwidGhyZWVEU2VjdXJlRW5hYmxlZCI6dHJ1ZSwicGF5cGFsRW5hYmxlZCI6dHJ1ZSwicGF5cGFsIjp7ImRpc3BsYXlOYW1lIjoiR3Jvb3ZpbmciLCJjbGllbnRJZCI6bnVsbCwicHJpdmFjeVVybCI6Imh0dHA6Ly9leGFtcGxlLmNvbS9wcCIsInVzZXJBZ3JlZW1lbnRVcmwiOiJodHRwOi8vZXhhbXBsZS5jb20vdG9zIiwiYmFzZVVybCI6Imh0dHBzOi8vYXNzZXRzLmJyYWludHJlZWdhdGV3YXkuY29tIiwiYXNzZXRzVXJsIjoiaHR0cHM6Ly9jaGVja291dC5wYXlwYWwuY29tIiwiZGlyZWN0QmFzZVVybCI6bnVsbCwiYWxsb3dIdHRwIjp0cnVlLCJlbnZpcm9ubWVudE5vTmV0d29yayI6dHJ1ZSwiZW52aXJvbm1lbnQiOiJvZmZsaW5lIiwidW52ZXR0ZWRNZXJjaGFudCI6ZmFsc2UsImJyYWludHJlZUNsaWVudElkIjoibWFzdGVyY2xpZW50MyIsImJpbGxpbmdBZ3JlZW1lbnRzRW5hYmxlZCI6dHJ1ZSwibWVyY2hhbnRBY2NvdW50SWQiOiJncm9vdmluZyIsImN1cnJlbmN5SXNvQ29kZSI6IkVVUiJ9LCJtZXJjaGFudElkIjoiMmpyNHo2cXo0cmY0bjV4dCIsInZlbm1vIjoib2ZmIn0=",
+            auth_key: undefined,
         }
     },
     methods: {
-        finishPayment() {
+        obtainInstance() {
+            braintree.client.create({
+            
+            authorization: this.auth_key
+            })
+            .then(clientInstance => {
+                
+                let options = {
+                    client: clientInstance,
+                    styles: {
+                        input: {
+                            'font-size': '15px',
+                            'font-family': 'Comic-Sans'
+                        }
+                    },
+                    fields: {
+                        number: {
+                            selector: '#number',
+                            placeholder: 'Enter Credit Card',
+                            maxCardLength: 16,
+                        },
+                        cvv: {
+                            selector: '#cvv',
+                            placeholder: 'CVV',
+                            maxlength: 3,
+                        },
+                        expirationMonth: {
+                            selector: '#expirationMonth',
+                            placeholder: 'MM',
+                            maxlength: 2,
+                        },
+                        expirationYear: {
+                            selector: '#expirationYear',
+                            placeholder: 'YYYY',
+                        }
+                    }
+                }
+                return braintree.hostedFields.create(options)
+                })
+                .then(hostedFieldInstance => {
+                    this.hostedFieldInstance = hostedFieldInstance;
+                    console.log(this.hostedFieldInstance)
+                })
+                .catch(err => {
+                    console.log(err)
+                });
             
         },
         payWithCreditCard() {
             if(this.hostedFieldInstance)
             {
                 this.hostedFieldInstance.tokenize().then(payload => {
-                    this.$emit('finishPayment', payload)
+                    this.$emit('finishPayment', payload.nonce)
+                    console.log(payload.nonce)
                 })
                 .catch(err => {
                     console.error(err);
@@ -59,64 +105,19 @@ export default {
             }
         }
     }, 
-    mounted() {
+    beforeMount() {
         var authorizedGAxios = GAxios;
         var GAxiosToken = this.gsecurity.getToken();
         authorizedGAxios.defaults.headers.common['Authorization'] = 'Token ' + GAxiosToken;
 
         authorizedGAxios.get(endpoints.braintree)
         .then(response => {
-            console.log(response)
+            this.auth_key = response.data
         }).catch(ex => {
             console.log(ex);
-        });
+        }).then(() => this.obtainInstance());        
+    },
 
-        braintree.client.create({
-            
-            authorization: this.auth_key
-        })
-        .then(clientInstance => {
-            
-            let options = {
-                client: clientInstance,
-                styles: {
-                    input: {
-                        'font-size': '15px',
-                        'font-family': 'Archivo'
-                    }
-                },
-                fields: {
-                    number: {
-                        selector: '#number',
-                        placeholder: 'Enter Credit Card',
-                        maxCardLength: 16,
-                    },
-                    cvv: {
-                        selector: '#cvv',
-                        placeholder: 'CVV',
-                        maxlength: 3,
-                    },
-                    expirationMonth: {
-                        selector: '#expirationMonth',
-                        placeholder: 'MM',
-                        maxlength: 2,
-                    },
-                    expirationYear: {
-                        selector: '#expirationYear',
-                        placeholder: 'YYYY',
-                    }
-                }
-            }
-            return braintree.hostedFields.create(options)
-            console.log('hi1' , options)
-        })
-        .then(hostedFieldInstance => {
-            this.hostedFieldInstance = hostedFieldInstance;
-        })
-        .catch(err => {
-            console.log(err)
-        });
-    }
 }
 </script>
 
