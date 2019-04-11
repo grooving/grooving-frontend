6ca829ba0aca89860ac86ba6d27d8950c36a76bf
Francisco de Paz <frandepaz@yahoo.es>
2019-04-11 02:03:12 +0200

    Added minimal css to terms text

--- src/components/TermsText.vue
+++ src/components/TermsText.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="content">
-    {{termAndCond}}
+
+      <span v-html="termAndCond"></span>
+
+    
+    
   </div>
 </template>
 
@@ -16,5 +20,50 @@ export default {
 }
 </script>
 
+<style>
+
+  h1 {
+    margin-top: 5%;
+    margin-bottom: 5%;
+  }
+
+  h2 {
+    margin-top: 5%;
+    margin-bottom: 3%;
+  }
+
+  p {
+    text-align: justify;
+    margin-right: 3%;
+    margin-left: 3%;
+
+  }
+
+    @media (min-width:768px)  {
+
+    h1 {
+      display: none;
+      margin-top: 5%;
+      margin-bottom: 5%;
+    }
+
+    h2 {
+      margin-top: 5%;
+      margin-bottom: 3%;
+    }
+
+    p {
+      text-align: justify;
+      margin-right: 3%;
+      margin-left: 3%;
+
+    }
+
+    }
+
+content {
+  text-align: left;
+
+}
 
 </style>
