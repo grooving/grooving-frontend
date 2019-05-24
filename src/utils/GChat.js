a9c3a8eda15707ab18ffd7dfdd7ab8f79c9bfa20
McAllisterDTur <Joseph.jmlc@gmail.com>
2019-05-24 02:07:42 +0200

    Arreglo del despliegue

--- src/utils/GChat.js
+++ src/utils/GChat.js
@@ -1,6 +1,6 @@
 
 // Server socket
-const HOST_URI = 'groovy-fupload-test.herokuapp.com';
+const HOST_URI = 'groovy-bupload-test.herokuapp.com';
 
 // Both normal and secure forms
 const SUPPORTED_PROTOCOLS = ['ws', 'wss']
